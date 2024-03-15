b0b1e1d623ae680e4fd2f6095f8d85c6969a2a6c
Mohd-AAT <157307764+Mohd-AAT@users.noreply.github.com>
2024-03-15 06:49:15 +0530

    Update 2_Calculator.js

--- 2_Calculator.js
+++ 2_Calculator.js
@@ -1,30 +1,39 @@
-let string ="";
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
 
-let arr = Array.from(buttons);
-arr.forEach((button) => {
-    button.addEventListener( 'click' , (ele)=>{
+const buttons = document.querySelectorAll('.button');
+const input = document.querySelector('.input');
+let str = "";
+let ans = "";
+
+Array.from(buttons).forEach( (button)=>{
+    button.addEventListener('click',(ele)=>{
+        console.log(ele.target.innerText);
         try{
-            if(ele.target.innerHTML == '='){
-                string = eval(string);
-                input.value = string;
+            if(ele.target.innerText == 'AC'){
+                str = "";
+                input.value = str;
             }
-            else if(ele.target.innerHTML == 'DEL'){
-                string = string.substring(0,string.length-1);
-                input.value = string;
+            else if(ele.target.innerText == 'DEL'){
+                if(str.length>0){
+                    str = str.slice(0,-1);
+                    input.value = str;
+                }
             }
-            else if(ele.target.innerHTML == 'AC'){
-                string = "";
-                input.value = string;
+            else if(ele.target.innerText == '='){
+                ans = eval(str);
+                str = 'Ans';
+                input.value = str;
+            }
+            else if(ele.target.innerText == 'Ans'){
+                str += ans.toString();
+                input.value = str;
             }
             else{
-                string += ele.target.innerHTML;
-                input.value = string;
+                str += ele.target.innerText;
+                input.value = str;
             }
         }
         catch(err){
-            alert("Invalid Input");
+            console.log(err);
         }
     })
 })
